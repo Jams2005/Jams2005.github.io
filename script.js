772a292435ff7925c5bc517ff834159d124e5cd2
Jams2005 <60399851+Jams2005@users.noreply.github.com>
2020-01-28 21:28:54 +0400

    1
    1

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
 var score = 0;
 var auto = 0;
-  var s1 = 10;
-  var cost1 = 300;
+var s1 = 10;
+var cost1 = 300;
+function updText() {
+  document.getElementsByTagName("p")[0].firstChild.data = "Money: " + score;
+  document.getElementsByTagName("p")[1].firstChild.data = "Auto: " + auto;
+  document.getElementsByTagName("s1")[0].firstChild.data = cost1;
+  document.getElementsByTagName("s2")[0].firstChild.data = cost2;
+  document.getElementsByTagName("s3")[0].firstChild.data = cost3;
+  document.getElementsByTagName("s4")[0].firstChild.data = cost4;
+  document.getElementsByTagName("s5")[0].firstChild.data = cost5;
+}
 function clickBtn() {
   score++;
   console.log(score);
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
-}
-function clickBtn1() {
-  auto++;
-  console.log(auto);
-  document.getElementsByTagName("p")[1].firstChild.data = "Clicks: " + auto;
+  updText();
 }
 function cps() {
   score = score + auto;
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
+  updText();
 }
 function S1() {
-
-  score = score - s1;
-  cost1 = cost1 + 10;
-  document.getElementsByTagName("s1")[0].firstChild.data = cost1;
+  if (cost1 < score ){
+    score = score - cost1;
+    cost1 = cost1 + 10;
+    auto = auto + s1;
+    updText()
 }
-function S2() {
-  var s2 = 1000;
-  score = score - s2;
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
-}
-function S3() {
-  var s3 = 10000;
-  score = score - s3;
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
-}
-function S4() {
-  var s4 = 100000;
-  score = score - s4;
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
-}
-function S5() {
-  var s5 = 500000;
-  score = score - s5;
-  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
 }
 
 setInterval(cps, 1000);
 
 function loadPage() {
+  updText();
   $("#background").animate({}, 800, function() {
     $("#clickbtn").animate({height: "250px", width: "250px"}, 850, function() {
       $("#clickbtn").animate({fontSize: "45px"}, 850, function() {
